bea317d883345c2b2791a09cccae441da0bfec0b
Naoufal <132677330+Naoufal-El@users.noreply.github.com>
2025-10-22 11:33:49 +0200

    Delete terminaApp_employee_mangement directory

--- terminaApp_employee_mangement/conn_db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def connect():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="admin12")
-    return conn
